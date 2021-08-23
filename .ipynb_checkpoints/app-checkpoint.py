545a73e5fd4c4e24113a16ecde68e8f6a9a45cc9
maniconaji <manuel.huerta.quian@gmail.com>
2021-08-23 18:08:15 -0400

    revision v2.4

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -10,13 +10,13 @@ import sys
 sys.path.insert(0, 'Script')
 from read_data import read_csvdata
 
-st.set_page_config(page_title = 'Desafió AInwater', layout = "wide", initial_sidebar_state = "auto")
+st.set_page_config(page_title = 'Desafío AInwater', layout = "wide", initial_sidebar_state = "auto")
 
 ########################################## sidebar #################################################
 
 sidebar = st.sidebar
 
-sidebar.title('Desafió AInwater')
+sidebar.title('Desafío AInwater')
 
 sidebar.markdown("""
 El siguiente desafío tiene por objetivo central caracterizar el funcionamiento de la planta durante una semana dividido en cuatro etapas. Para esto, cuenta con variables que describen n° de registro de la medición, fecha, hora, el consumo energético del motor del soplador, nivel de oxígeno, nivel de agua y una identificación del ciclo a que corresponde. 
@@ -71,6 +71,8 @@ if variable == "Motor soplador":
     Por otro lado, cuando se estudia los Hz totales por cada ciclo, se deduce:
     * Hay un mayor consumo energético en los ciclos correspondientes a las tardes-noches (últimos tres ciclos de cada día).
     * Este fenómeno es producto del aumento en el flujo de RIL a tratar en la planta de tratamiento.
+    
+    Estas aseveraciones son comprobables en el siguiente gráfico de barras, que corresponde a los Hz totales por cada ciclo, considerando distintos colores para cada día.
     """)
     fig = bar_by_cycle_sum(
     df, col_name, title = title, col_color = "day", col_hover_data = ["do_level", "h2o_level", "month", "year"], height = 400, width=1100
@@ -80,6 +82,14 @@ if variable == "Motor soplador":
 elif variable == "Oxígeno":
     col_name = 'do_level'
     
+    container_0 = st.beta_container()
+    container_0.header(variable)
+    container_0.markdown("""
+    La variable "do_level" contiene el nivel de oxígeno a lo largo de esta semana, caracterizándose por presentar una mayor variabilidad con relación a la media a la hora de comparar el promedio y la desviación estándar de esta variable. Esto se debe principalmente a que la inyección de oxígeno en la etapa aerobia de la planta de tratamiento no es continuo, sino bajo un sistema batch (observable al manejar la barra de abajo de la serie de tiempo).
+    """)
+    container_0.subheader("Resumen estadistico")
+    container_0.write(df.filter([col_name]).describe().T)
+    
     # Serie de Tiempo   
     container_1 = st.beta_container()
     fig = px.line(df, x="datetime", y=col_name, color="cycle_id", title='<b>Serie de Tiempo del nivel de oxígeno</b>')
@@ -87,13 +97,16 @@ elif variable == "Oxígeno":
     fig.update_xaxes(rangeslider_visible=True)
     container_1.plotly_chart(fig, use_container_width=True)
     
-#     fig1, grouped_df, col_name = bar_by_cycle(df, variable, height = 500, width=1000)
-
-#     if variable in ["Oxígeno", "Agua"]:
-#         container_2 = st.beta_container()
-
-#         fig2, grouped_df, col_name = bar_by_cycle_with_query(df, variable, query_text = "blower_hz > 0", height = 500, width=1000)
-#         container_2.plotly_chart(fig2, use_container_width=True)
-    
+    #
+    container_2 = st.beta_container()
+    title = {
+    "blower_hz"  : "<b>Nivel medio de Hz</b>",
+    "do_level"   : "<b>Nivel medio de O<sub>2</sub></b>",
+    "h2o_level"  : "<b>Nivel medio de H<sub>2</sub>O</b>"
+    }
+    fig1 = bar_by_cycle_mean(df, col_name, title = title, height = 400, width=1200)
+    container_1.plotly_chart(fig1, use_container_width=True)
+    fig2 = bar_by_cycle_mean(df, col_name, title = title, query_text = "blower_hz > 0", height = 400, width=1200)
+    container_1.plotly_chart(fig2, use_container_width=True)
 
 
